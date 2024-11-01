38c00914c408f0bccaafa361038403ea17e552f2
Titus Njema <njema3@gmail.com>
2024-11-01 12:14:04 +0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+const Hero = () => {
+  return (
+    <div class="title">
+        <div class="top"><h3>Your New Shopping Experience!</h3></div>
+        <div class="bottom">
+            <p>Shop In a Style</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
