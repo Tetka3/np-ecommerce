b39979f9e91b7d637e9190f63b5119cc3054b08b
Titus Njema <njema3@gmail.com>
2024-11-01 12:14:45 +0300

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
