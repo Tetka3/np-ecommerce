58fc5ee7b96f6a24bf01f3990dba89b1abb5738d
Titus Njema <njema3@gmail.com>
2024-11-01 12:07:49 +0300

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,30 @@ import './style.css';
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div class="nav-bar">
+        <div class="logo">
+            <img src="./images/shop.jpeg" />
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Search item here"/>
+            <button>Go</button>
+        </div>
+        <div class="add">
+            <div class="links">
+                <ul>
+                    <li>My Account</li>
+                    <li>Register</li>                     
+                    <li>Help </li> 
+                </ul>
+            </div>
+            <div class="cart"><a href="cart.html">
+                <img src="./images/cart.jpeg" />
+                <span class="js-quantity">0</span></a>
+            </div>            
+        </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
 
