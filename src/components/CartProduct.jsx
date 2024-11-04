a4211b6dd8a35f8c45aaaca56dc29eaf9074a9db
Titus Njema <njema3@gmail.com>
2024-11-04 18:34:33 +0300

    first commit

---
+++ src/components/CartProduct.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CartProduct = () => {
+  return (    
+    <div class="product-details">
+        <div class="cart-image">
+            <img src="" />
+        </div>
+        <div class="specifics">
+            <p>Black suit</p>
+            <p>$20.00</p>
+            <p>Quantity: 1</p>
+        </div>
+    </div>  
+  )
+}
+
+export default CartProduct
