cf426ae662d9e5e54715796d66bef1afc217cf00
Titus Njema <njema3@gmail.com>
2024-11-04 19:26:19 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './pages/Home';
-// import Cart from "./pages/Cart";
+import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path='/np-ecommerce' element={<Home />} />
-      {/* <Route path="/np-ecommerce/cart" element={<Cart />} />      */}
+      <Route path="/np-ecommerce/cart" element={<Cart />} />     
       <Route path="/np-ecommerce/login" element={<Login />} />     
     </>
   )
